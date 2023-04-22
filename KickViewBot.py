8fb5df1b835d565c4f05105cc94c441aad8f3c38
AnonX <64300849+Infinity242@users.noreply.github.com>
2023-04-22 03:57:41 -0500

    Update KickViewBot.py

--- KickViewBot.py
+++ KickViewBot.py
@@ -16,7 +16,6 @@ class KickBot():
         chrome_options = Options()
         chrome_options.add_argument("--start-minimized")
         chrome_options.add_argument("--mute-audio")
-        #chrome_options.add_argument("--headless")
         self.driver = Chrome(options=chrome_options)
 
     def doTest(self):
@@ -25,14 +24,14 @@ class KickBot():
         not_found_count = 0
         while True:
             try:
-                # Locate the button by its text
+                
                 button = self.driver.find_element(By.XPATH, "//button[contains(text(), 'Start watching')]")
-                # Click the button
+                
                 button.click()
                 print("Button clicked!")
                 not_found_count = 0
             except NoSuchElementException:
-                # Refresh page if "Oops, Something went wrong" message is on the screen
+                
                 if "Oops, Something went wrong" in self.driver.page_source:
                     self.driver.refresh()
                     print("Page refreshed!")
@@ -75,43 +74,34 @@ class App(Frame):
         self.kick_bots = []
 
     def create_widgets(self):
-        # Create a frame for the header
+        
         header_frame = Frame(self, bg="#4E4E4E", height=40)
         header_frame.pack(fill=X)
 
-        # Add a label for the header text
         header_label = Label(header_frame, text="Kick View Bot", font=("Segoe UI", 14), fg="#FFFFFF", bg="#4E4E4E")
         header_label.pack(side=LEFT, padx=10)
 
-        # Create a frame for the content
         content_frame = Frame(self, bg="#2D2D2D")
         content_frame.pack(fill=BOTH, expand=True, pady=10, padx=20)
 
-        # Add a label for the URL input
         url_label = Label(content_frame, text="Enter website URL:", font=("Segoe UI", 10), fg="#FFFFFF", bg="#2D2D2D")
         url_label.grid(row=0, column=0, pady=5)
 
-        # Add an entry field for the URL input
         self.url_input = Entry(content_frame, bg="#1E1E1E", fg="#FFFFFF", font=("Segoe UI", 10), borderwidth=0)
         self.url_input.grid(row=1, column=0, padx=10)
 
-        # Add a label for the tabs input
         tabs_label = Label(content_frame, text="Enter number of tabs to open:", font=("Segoe UI", 10), fg="#FFFFFF", bg="#2D2D2D")
         tabs_label.grid(row=2, column=0, pady=5)
 
-        # Add an entry field for the tabs input
         self.tabs_input = Entry(content_frame, bg="#1E1E1E", fg="#FFFFFF", font=("Segoe UI", 10), borderwidth=0)
         self.tabs_input.grid(row=3, column=0, padx=10)
 
-        # Add a start button
         start_button = Button(content_frame, text="Start Bot", font=("Segoe UI", 10), fg="#FFFFFF", bg="#43B581", activebackground="#43B581", borderwidth=0, command=self.start_bot)
         start_button.grid(row=4, column=0, pady=10)
 
-        # Add a stop button
         stop_button = Button(content_frame, text="Stop Bot", font=("Segoe UI", 10), fg="#FFFFFF", bg="#F04747", activebackground="#F04747", borderwidth=0, command=self.stop_bot)
         stop_button.grid(row=5, column=0, pady=10)
 
-        # Add a label for the credits
         credits_label = Label(self, text="Made By: AnonX", font=("Segoe UI", 10), fg="#FFFFFF", bg="#1E1E1E")
         credits_label.pack(side=BOTTOM, padx=10, pady=10, anchor=SE)
 

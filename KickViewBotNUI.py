0431f850cbc45bfe227d1333a02d2b3a14ea3609
Greg P <gorgarp@gmail.com>
2023-06-24 22:21:34 -0500

    Update KickViewBotNUI.py
    Fix:
    
    KickViewBot-main\KickViewBot-main\KickViewBotHEADLESS.py", line 17, in setupWebBrowser
        chrome_options = Options()
                         ^^^^^^^
    NameError: name 'Options' is not defined

--- KickViewBotNUI.py
+++ KickViewBotNUI.py
@@ -1,11 +1,12 @@
-from selenium.webdriver import Chrome
+from undetected_chromedriver import ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.chrome.options import Options
 from threading import Thread
 import undetected_chromedriver as uc
 from time import sleep
 from multiprocessing import Process
+from selenium.webdriver import Firefox, FirefoxOptions
+import requests
 
 class KickBot():
     def __init__(self, url):
@@ -13,26 +14,32 @@ class KickBot():
         self.driver = None
 
     def setupWebBrowser(self):
-        chrome_options = Options()
-        chrome_options.add_argument("--start-minimized")
+        chrome_options = ChromeOptions()
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--mute-audio")
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
         self.driver = uc.Chrome(options=chrome_options)
+
+    def getToken(self):
+        referer = self.driver.current_url
+        response = requests.get(referer)
+        csrf_token = response.headers.get('X-CSRFToken')
+        return csrf_token
 
     def doTest(self):
         self.setupWebBrowser()
         self.driver.get(self.url)
         not_found_count = 0
+        csrf_token = self.getToken()
         while True:
             try:
-
                 button = self.driver.find_element(By.CSS_SELECTOR, "button.variant-action.size-sm")
-
                 button.click()
                 print("Button clicked!")
                 not_found_count = 0
             except NoSuchElementException:
-
                 if "Oops, Something went wrong" in self.driver.page_source:
                     self.driver.refresh()
                     print("Page refreshed!")
@@ -46,11 +53,13 @@ class KickBot():
                     self.setupWebBrowser()
                     self.driver.get(self.url)
                     print("Browser restarted")
+                    csrf_token = self.getToken()
                 else:
                     print("Watch now button not found on this page")
                 not_found_count += 1
             sleep(10)
 
+        self.driver.execute_script("document.querySelector('#vjs_video_3 > div:nth-child(1) > video').style.display = 'none'")
 
         while True:
             pass
@@ -66,13 +75,11 @@ def run_script(url, num_threads):
         num_windows += 1
         if num_windows == 4:
             num_windows = 0
-            sleep(6) 
+            sleep(6)
     for process in processes:
         process.join()
 
 if __name__ == '__main__':
-    print("This tool was created by AnonX#8622")
-    input("Press Enter to say thanks...")
     url = input("Enter the website URL: ")
     num_threads = int(input('Enter the number of threads to use: '))
     run_script(url, num_threads)
